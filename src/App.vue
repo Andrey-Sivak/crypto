6d143ad935e9057b9ddb7e6c41a13e0d23cf0e52
a.sivak <riziy_bes@mail.ru>
2022-01-05 02:09:11 +0300

    wrote common app logic

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,292 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+    <div v-if="loading" class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
+      <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+           viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+    </div>
+    <div class="container">
+      <section>
+        <div class="flex">
+          <div class="max-w-xs">
+            <label for="wallet" class="block text-sm font-medium text-gray-700"
+            >Тикер</label
+            >
+            <div class="mt-1 relative rounded-md shadow-md">
+              <input
+                      v-model="ticker"
+                      @keyup="handleTickerInput"
+                      @keypress.enter="addTicker(ticker)"
+                      type="text"
+                      name="wallet"
+                      id="wallet"
+                      class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+                      placeholder="Например DOGE"
+              />
+            </div>
+            <div v-if="tips.length" class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+              <span v-for="tip in tips"
+                    :key="tip"
+                    @click="addTicker(tip)"
+                    class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+                {{ tip }}
+              </span>
+            </div>
+            <div v-if="error" class="text-sm text-red-600">{{ error }}</div>
+          </div>
+        </div>
+        <button
+                @click="addTicker(ticker)"
+                type="button"
+                class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
+          <!-- Heroicon name: solid/mail -->
+          <svg
+                  class="-ml-0.5 mr-2 h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 24 24"
+                  fill="#ffffff"
+          >
+            <path
+                    d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+            ></path>
+          </svg>
+          Добавить
+        </button>
+      </section>
+
+      <template v-if="tickers.length">
+        <hr class="w-full border-t border-gray-600 my-4"/>
+        <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          <div
+                  v-for="t in tickers"
+                  :key="t"
+                  @click="selectActiveTicker(t)"
+                  :class="{
+                    'border-purple-800': t === activeTicker
+                  }"
+                  class="bg-white overflow-hidden border-4 shadow rounded-lg border-solid cursor-pointer"
+          >
+            <div class="px-4 py-5 sm:p-6 text-center">
+              <dt class="text-sm font-medium text-gray-500 truncate">
+                {{ t.name }} - USD
+              </dt>
+              <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                {{ t.price }}
+              </dd>
+            </div>
+            <div class="w-full border-t border-gray-200"></div>
+            <button
+                    @click.stop="removeTicker(t)"
+                    class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none">
+              <svg
+                      class="h-5 w-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="#718096"
+                      aria-hidden="true">
+                <path
+                        fill-rule="evenodd"
+                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                        clip-rule="evenodd"></path>
+              </svg>
+              Удалить
+            </button>
+          </div>
+        </dl>
+        <hr class="w-full border-t border-gray-600 my-4"/>
+        <section v-if="activeTicker" class="relative">
+          <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+            {{ activeTicker.name }} - USD
+          </h3>
+          <div class="flex items-end border-gray-600 border-b border-l h-64">
+            <div v-for="(bar, idx) in normalizeGraph()"
+                 :key="idx"
+                 :style="{ height: `${bar}%` }"
+                 class="bg-purple-800 border w-10"></div>
+          </div>
+          <button
+                  @click="unselectActiveTicker"
+                  type="button"
+                  class="absolute top-0 right-0"
+          >
+            <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xmlns:svgjs="http://svgjs.com/svgjs"
+                    version="1.1"
+                    width="30"
+                    height="30"
+                    x="0"
+                    y="0"
+                    viewBox="0 0 511.76 511.76"
+                    style="enable-background:new 0 0 512 512"
+                    xml:space="preserve"
+            >
+          <g>
+            <path
+                    d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                    fill="#718096"
+                    data-original="#000000"
+            ></path>
+          </g>
+        </svg>
+          </button>
+        </section>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+    export default {
+        name: "App",
+        data() {
+          return {
+              loading: true,
+              ticker: null,
+              tickers: [],
+              activeTicker: null,
+              error: false,
+              allTickers: [],
+              tips: [],
+
+              graph: [],
+          }
+        },
+        methods: {
+            addTicker(ticker) {
+
+                if (!ticker) return;
+
+                this.isTickerAlreadyAdd(ticker);
+                if (this.error) return;
+
+                this.isTickerValid(ticker);
+                if (this.error) return;
+
+                const newTicker = {
+                    name: ticker.toUpperCase(),
+                    price: '-',
+                };
+
+                this.tickers.push(newTicker);
+                this.clearTickerInput();
+                this.error = false;
+                this.tips = [];
+            },
+
+            normalizeGraph() {
+              const maxGraphValue = Math.max(...this.graph);
+              const minGraphValue = Math.min(...this.graph);
+
+              return this.graph.map(
+                  price => 5 + ((price - minGraphValue) * 95) / (maxGraphValue - minGraphValue)
+              );
+            },
+
+            clearTickerInput() {
+                this.ticker = '';
+            },
+
+            isTickerAlreadyAdd(ticker) {
+                this.error = this.tickers.find(t => t.name === ticker.toUpperCase()) ? 'Такой тикер уже добавлен' : false;
+            },
+
+            isTickerValid(ticker) {
+                this.error = this.allTickers.includes(ticker.toUpperCase()) ? false : 'Тикер не найден в списке';
+            },
+
+            tipsTicker(string) {
+                let count = 0;
+                this.tips = this.allTickers.filter((t) => {
+                    if (!string) return;
+
+                    if (count >= 4) return;
+
+                    if (t.includes(string.toUpperCase())) {
+                        count++;
+                        return t;
+                    }
+                });
+            },
+
+            removeTicker(tickerToRemove) {
+                console.log(tickerToRemove, this.activeTicker);
+
+                if (tickerToRemove === this.activeTicker) {
+                    this.activeTicker = null;
+                    this.graph = [];
+                }
+
+                this.tickers = this.tickers.filter(t => t !== tickerToRemove);
+            },
+
+            handleTickerInput() {
+                this.isTickerAlreadyAdd(this.ticker);
+                this.tipsTicker(this.ticker);
+                // TODO: to upper
+                // this.ticker.toUpperCase();
+            },
+
+            selectActiveTicker(t) {
+                this.activeTicker = t;
+                this.graph = [];
+            },
+
+            unselectActiveTicker() {
+                this.activeTicker = null;
+            },
+
+            setPrice(ticker) {
+                if(!ticker[1].USD) return;
+
+                const currentTicker = this.tickers.find(t => t.name === ticker[0]);
+
+                currentTicker.price = ticker[1].USD > 1 ? ticker[1].USD.toFixed(2) : ticker[1].USD.toPrecision(2);
+
+                if (!this.activeTicker) return;
+                if (ticker[0] === this.activeTicker.name) this.graph.push(currentTicker.price);
+            },
+
+            async getPrice(tickers) {
+                if (!tickers.length) return;
+
+                const tickersString = tickers.length !== 1 ? tickers.map(t => t.name).join(',') : tickers[0].name;
+
+                const url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${tickersString}&tsyms=USD`;
+
+                await fetch(url)
+                    .then(res => res.json())
+                    .then(data => Object.entries(data))
+                    .then(tickersData => {
+                        tickersData.forEach(t => this.setPrice(t));
+                    })
+            }
+        },
+
+        async created() {
+            const url = 'https://min-api.cryptocompare.com/data/all/coinlist';
+
+            await fetch(url)
+                .then(res => res.json())
+                .then(data => this.allTickers = Object.keys(data.Data).sort((a, b) => a.length - b.length));
+
+            setInterval(() => {
+                this.getPrice(this.tickers);
+            }, 3000)
+        },
+
+        mounted() {
+            this.loading = false;
+        },
+    };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./app.css"></style>
